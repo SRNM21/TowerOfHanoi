859ed80859269e659b9a2b366c6b97db811d4b24
SRNM21 <gregorio1nonly@gmail.com>
2023-10-23 22:21:05 +0800

    test

--- Game/Game.java
+++ Game/Game.java
@@ -259,6 +259,7 @@ public class Game implements KeyListener
          * Prevents:
          *      place disk on the same peg 
          *      place disk on the top of the smaller disk
+         * 
          */
         if ((peg == prevPeg) || (peg.getDiskCount() > 0 && (currentDisk.getStatus() > peg.peek().getStatus()))) return;
 
